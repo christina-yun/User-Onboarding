581e9513a0ef61a41fbdedb9ac4c9968f545a2a1
christina-yun <poncheeyun@yahoo.com>
2021-08-18 15:07:33 -0700

    Finished the form part with validation and notes -- currently need to render the new user onscreen

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
 
 //displays the form
 import Form from './Form';
 //displays the submitted results from Form
 import SubmittedForm from './SubmittedForm';
+//validation piece
+import formSchema from './formSchema';
+
 import './App.css';
 
 //Making these starting states globally accessible
@@ -25,18 +30,74 @@ const initialUserList = [];
 
 const initialDisabled = true;
 
+
 function App() {
   //set up states for: form values, form errors (when users do things wrong/don't follow directions), pushing/displaying newly-created users into the initialUserList array, disabling the submit button
   const [formValues, setFormValues] = useState(initialFormValues);
 
   const [formErrors, setFormErrors] = useState(initialFormErrors);
 
-  const [user, setUser] = useState(initialUserList);
+  const [users, setUsers] = useState(initialUserList);
 
   const [disabled, setDisabled] = useState(initialDisabled);
 
-  
+  const getUsers = () =>{
+    //need to retrieve list of users each time this is invoked
+    axios.get('https://reqres.in/api/users')
+    .then(res => {
+      setUsers(res.data.data)
+    })
+    .catch(err => console.error(err))
+    
+  }
+  const postNewUser = newUser => {
+    //before we can submit, we need to know where the submittal is getting posted -- the API in this case that will create an object with the responses, and then clear the form
+    axios.post('https://reqres.in/api/users', newUser)
+    .then(response => {
+      console.log('i am response', response.data)
+    })
+    .catch(err => {
+      console.error(err);
+    })
+    setFormValues(initialFormValues);
+  }
 
+  const validated = (name, value) => {
+    //need a way to run all the values through the schema
+    //note: reach takes 2 inputs. Name is used to know WHICH things to validate per value
+    yup.reach(formSchema, name).validate(value)
+    .then(() => setFormErrors({...formErrors, [name]: '' })) 
+    .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]}))
+  }
+
+  const inputChange = (name, value) => {
+  //need something that will pick up the inputs and update them after they're validated through schema
+  validated(name, value);
+  setFormValues({
+    ...formValues, [name]: value
+  })
+  }
+  
+  //Calling getUsers to set the initial information and make it available
+  useEffect(() => {
+    getUsers();
+  },[])
+
+    //this is to tell the submit button when it is no longer disabled
+  useEffect(() => {
+    formSchema.isValid(formValues).then(valid => setDisabled(!valid))
+  },[formValues])
+
+  function formSubmit(){
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password:formValues.password.trim(),
+      tos: formValues.tos
+    }
+    //invoke the request to send information to the API
+    postNewUser(newUser);
+  }
 
   return (
     <div className="App">
@@ -44,6 +105,8 @@ function App() {
       values={formValues} 
       disabled={disabled}
       errors={formErrors}
+      submit={formSubmit}
+      change={inputChange}
       />
     </div>
   );
--- onboarding/src/Form.js
+++ onboarding/src/Form.js
@@ -2,24 +2,37 @@ import React from 'react';
 
 function Form(props){
     //passing all the props needed
-    const { values, disabled, errors } = props;
+    const { values, disabled, errors, submit, change } = props;
     
     //onSubmit function that takes in submit from its parent
     const onSubmit = event =>{
     //stops the page from reloading
         event.preventDefault();
     //invokes what happens at submittal
-        
+        submit(change);
     }
 
     const onChange = event => {
-        console.log(event.target.value)
+        //destructuring the event.target values available to match or change
+        const { type, name, value, checked } = event.target;
+        //calling out specifically that 'checkbox' utilizes something other than 'value'
+        const valueToUse = type === 'checkbox' ? checked : value;
+    //a function needs to execute to update the state from just inputs to actual state
+        change(name, valueToUse);
     }
     
     return (
         <div>
             <h3>Create Account</h3>
             <form className='form-container' onSubmit={onSubmit}>
+                
+                <div className='errors'>
+                    <div>{errors.name}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.password}</div>
+                    <div>{errors.tos}</div>
+                </div>
+
                 <div className='form-inputs'>
                     <label> Name: 
                         <input 
